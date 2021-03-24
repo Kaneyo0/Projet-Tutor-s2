6fc05a9e64b1236677d3c9db3d33bfbef82b1493
Benjamin-Rdg <81095896+Benjamin-Rdg@users.noreply.github.com>
2021-03-24 21:14:21 +0100

    Code du bot Discord
    Ajout de la fonctionnalité de "commande prefix"

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-
 import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
+
 load_dotenv(dotenv_path="config")
 
 
 class Bot(commands.Bot, discord.Client):
 
     def __init__(self):
-        super().__init__(command_prefix=";")
+        super(Bot, self).__init__(command_prefix=";")
+        self.add_command(commands.Command(self.poisson, name="poisson"))
 
     async def on_ready(self):
         print(f"{self.user.display_name} est connecté")
 
-    async def on_disconnect(self):
-        print(f"{self.user.display_name} c'est déconnecté")
+    async def poisson(self, msg):
+        file = discord.File("media\poisson.jpg", filename="poisson.jpg")
+        await msg.channel.send("Neuneuil : ", file=file)
 
-    async def on_message(self,msg):
-        if(msg.content.lower() == "poisson"):
-            file = discord.File("media\poisson.jpg", filename="poisson.jpg")
-            await msg.channel.send("Neuneuil : ", file=file)
-        if (msg.content.lower() == "sah"):
-            await msg.channel.send("Quelle plaisir")
+
 
 disc_Bot = Bot()
 disc_Bot.run(os.getenv("TOKEN"))
