0172e6f8fd54005f0d1bfc2edfcde2442e9024b2
Tom DUCHESNE <80764538+Kaneyo0@users.noreply.github.com>
2021-04-17 15:48:13 +0200

    Led clignote + bouton pour arrêter

--- amogus.py
+++ amogus.py
@@ -1,15 +1,21 @@
 import RPi.GPIO as GPIO
 from time import sleep
+
+GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(32,GPIO.OUT,initial=GPIO.LOW)
-GPIO.setup(31,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-while True:
-    etat = GPIO.input(31)
-    if etat == 1:
-        print("pushed")
-        GPIO.output(32,GPIO.HIGH)
-        sleep(1)
-        GPIO.output(32,GPIO.LOW)
-        sleep(1)
-    
+
+bouton=23
+led=5
+GPIO.setup(led,GPIO.OUT)
+GPIO.setup(bouton,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+while GPIO.input(bouton) == 1 :   
+    buttonState = GPIO.input(bouton)
+    GPIO.output(led, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(led, GPIO.LOW)
+    sleep(1)
+
+
+
